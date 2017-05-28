45a0ea3406a405fbbe6823b8bbf68dde1a84df62
Takeru SHIMIZU <s15t233@stu.kagawa-u.ac.jp>
2017-05-28 13:36:41 -0400

    Add Controllers

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,9 @@ defmodule Librarian.Router do
   scope "/", Librarian do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", BookController, :index
+    resources "/genres", GenreController
+    resources "/books",  BookController
   end
 
   # Other scopes may use custom stacks.
